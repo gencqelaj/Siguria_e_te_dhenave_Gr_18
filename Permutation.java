bd92926381c7db7578a992d24bb1cc75b9fe7d83
kushtrimhaziri <58092838+kushtrimhaziri@users.noreply.github.com>
2020-03-21 02:07:08 +0100

    Update Permutation.java

--- Permutation.java
+++ Permutation.java
@@ -4,12 +4,13 @@ public class Permutation {
     String name = "kushtrim";
 
     public void encrypt(String string) {
-       
+       //kodi per enkriptim 
         System.out.println("Teksti u enkriptua");
     }
 
     public void decrypt(String string) {
    
+        //kodi per dekriptim
         System.out.println("Teksti u dektriptua");
 
     }
