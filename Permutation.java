15d97acdf71d5911676a96eb3af06e9649e975d8
kushtrimhaziri <58092838+kushtrimhaziri@users.noreply.github.com>
2020-03-21 02:06:03 +0100

    Add files via upload

---
+++ Permutation.java
@@ -0,0 +1,18 @@
+
+public class Permutation {
+
+    String name = "kushtrim";
+
+    public void encrypt(String string) {
+       
+        System.out.println("Teksti u enkriptua");
+    }
+
+    public void decrpyt(String string) {
+   
+        System.out.println("Teksti u dektriptua");
+
+    }
+
+
+}
