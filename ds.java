2c6d299d8c603bcf629dd529587a206609836c68
visarahmeti <58608274+Visskey@users.noreply.github.com>
2020-03-26 21:23:40 +0100

    Add files via upload

---
+++ ds.java
@@ -0,0 +1,28 @@
+import java.sql.SQLOutput;
+        import java.util.InputMismatchException;
+        import java.util.Scanner;
+
+public class ds {
+
+    public static void main(String[] args) {
+        if ("permutation".equals(args[0])) {
+            Permutation permutation = new Permutation(args[2],args[3]);
+            if ("encrypt".equals(args[1])) {
+                permutation.encrypt();
+            } else if ("decrypt".equals(args[2])) {
+                permutation.decrpyt();
+            }
+        }
+
+        if ("morse".equals(args[0])) {
+
+            if ("encrypt".equals(args[1])) {
+                MorseEncoder encoder=new MorseEncoder(args[2]);
+                encoder.OutputMorse();
+
+            } else if ("decrypt".equals(args[1])) {
+                MorseDecoder decoder=new MorseDecoder(args[2]);
+
+            }
+        }
+    }}
