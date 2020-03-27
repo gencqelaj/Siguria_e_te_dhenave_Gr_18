89b3d2d005799d6d4dc84f2442dbb95b0d3f5d1a
kushtrimhaziri <58092838+kushtrimhaziri@users.noreply.github.com>
2020-03-27 17:54:51 +0100

    Update Permutation.java

--- Permutation.java
+++ Permutation.java
@@ -6,56 +6,52 @@ public class Permutation {
 
     private String key;
     private String teksti;
-
+    private int celesi ;
+    private int length;
+    private int b;
+    private String replaceString;
+    private ArrayList<Integer> array ;
+    private StringBuilder sb;
+    private List<String>lista;
     public Permutation(String key, String teksti)
     {
         this.key = key;
         this.teksti=teksti;
-    }
-
-    public void encrypt() {
-
-
-        int celesi = Integer.parseInt(key);
-
-        int length = String.valueOf(celesi).length();
-
-        int b = celesi;
-        ArrayList<Integer> array = new ArrayList<Integer>();
+        celesi = Integer.parseInt(key);
+        length = String.valueOf(celesi).length();
+        b=celesi;
+        array = new ArrayList<Integer>();
         do{
             array.add(b % 10);
             b /= 10;
         } while  (b > 0);
-
-        String replaceString = teksti.replaceAll(" ", "");             //replaces all occurrences of "a" to "e"
-        StringBuilder sb = new StringBuilder();
+            sb = new StringBuilder();
+         replaceString = teksti.replaceAll(" ", "");
         sb.append(replaceString);
 
-        //Shto "W" nese blloku eshte i paplotesuar
         while (sb.length() % length != 0) {
             sb.append("w");
 
+
         }
-        System.out.println(sb);
+        lista = splitToChar(sb, length);
+    }
 
-        List<String>lista = splitToChar(sb, length);
-
+    public void encrypt() {
         System.out.print("plaintext: " + " ");
         for(String name:lista) {
             System.out.print(name+" ");
         }
-
         System.out.println();
         System.out.print("key      : " + " ");
         for (int i = 0; i < lista.size(); i++) {
             System.out.print(celesi + " ");
-
         }
 
         String[] myArray = new String[lista.size()];
         lista.toArray(myArray);
 
-
+        String[] cifra = new String[lista.size()];
         System.out.println();
         System.out.print("ciphertext: ");
         for (int i = 0 ; i<lista.size();i++)
@@ -65,110 +61,54 @@ public class Permutation {
             for (int j =length ; j > 0 ; j--)
             {
                 sb1.append(myArray[i].charAt(array.get(j-1)-1));
-
             }
+            cifra[i]=sb1.toString();
             System.out.print(sb1+" ");
         }
 
-
+        System.out.println("ciphertext: ");
+        for (int i =0 ; i<cifra.length ; i++)
+        {
+            System.out.print(cifra[i]);
+        }
     }
 
-
-
-
-
-
-
-
-
-
-
-
     public void decrpyt() {
-
-        int celesi = Integer.parseInt(key);
-
-        int length = String.valueOf(celesi).length();
-
-        int b = celesi;
-        ArrayList<Integer> array = new ArrayList<Integer>();
-        do {
-            array.add(b % 10);
-            b /= 10;
-        } while (b > 0);
-        String replaceString = teksti.replaceAll(" ", "");             //replaces all occurrences of "a" to "e"
-        StringBuilder sb = new StringBuilder();
-        sb.append(replaceString);
-
-        //Shto "W" nese blloku eshte i paplotesuar
-        while (sb.length() % length != 0) {
-            sb.append("w");
-
-
-        }
-
-        List<String> lista = splitToChar(sb, length);
         String[] myArray = new String[lista.size()];
         lista.toArray(myArray);
 
-
-        System.out.println();
         System.out.println("ciphertext: ");
 
         Integer array1[] = array.toArray(new Integer[0]);
         reverse(array1);
 
-        String[] cifra = new String[lista.size()];
-        int i2 = 0;
-        int i3;
-
         for (int i = 0; i < lista.size(); i++) {
             StringBuilder sb1 = new StringBuilder();
             for (int j = 0; j <length; j++) {
                 int a= findIndex(array1,j+1);
-                    sb1.append(myArray[i].charAt(a));
-                    
-            }       
-			cifra[i]=sb.toString();
-                System.out.print(sb1 + " ");
-                
-
-
-
-        }   System.out.println();
-        System.out.println();
-		   System.out.println();
-		   System.out.println("ciphertext: ");
-        for (int i =0 ; i<cifra.length ; i++)
-        {
-            System.out.print(cifra[i]);
+                sb1.append(myArray[i].charAt(a));
+            }
+            System.out.print(sb1);
         }
-
-
+        System.out.println();
     }
-   public static void reverse(Integer a[])
+    
+    public static void reverse(Integer a[])
     {
         Collections.reverse(Arrays.asList(a));
-
     }
 
     public static int findIndex(Integer arr[], int t)
     {
-
-        // if array is Null
         if (arr == null) {
             return -1;
         }
-
-        // find length of array
+        
         int len = arr.length;
         int i = 0;
-
-        // traverse in the array
+        
         while (i < len) {
-
-            // if the i-th element is t
-            // then return the index
+            
             if (arr[i] == t) {
                 return i;
             }
@@ -188,6 +128,4 @@ public class Permutation {
         }
         return parts;
     }
-
-
 }
