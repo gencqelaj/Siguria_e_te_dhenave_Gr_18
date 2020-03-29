f730d03c5d137933eb61b5985a847ce3293bb5d5
kushtrimhaziri <58092838+kushtrimhaziri@users.noreply.github.com>
2020-03-29 20:51:22 +0200

    Update ds.java

--- ds.java
+++ ds.java
@@ -1,24 +1,48 @@
 public class ds {
 
     public static void main(String[] args) {
-        if ("permutation".equals(args[0])) {
-            Permutation permutation = new Permutation(args[2],args[3]);
-            if ("encrypt".equals(args[1])) {
-                permutation.encrypt();
-            } else if ("decrypt".equals(args[1])) {
-                permutation.decrpyt();
-            }
+        try {
+            switch (args[0]) {
+                case "permutation":
+                    switch (args[1]) {
+                        case "encrypt":
+
+                            Permutation permutation = new Permutation(args[2], args[3]);
+
+                            permutation.encrypt();
+                            break;
+                        case "decrypt":
+
+                            Permutation permutation1 = new Permutation(args[2], args[3]);
+                            permutation1.decrypt();
+                            break;
+                        default:
+                            System.out.println("Zgjedh si argument te dyte encrypt ose decrypt");
+                    }
+                case "morse":
+                    switch (args[1]) {
+                        case "encrypt":
+                            MorseEncoder encoder = new MorseEncoder(args[2]);
+                            encoder.OutputMorse();
+                            break;
+                        case "decrypt":
+                            MorseDecoder decoder = new MorseDecoder(args[2]);
+                            break;
+                        default:
+                            System.out.println("Zgjedh si argument te dyte encrypt ose decrypt");
+                    }
+                    break;
+                default:
+                    System.out.println("Argumenti i pare eshte gabim , zgjedh permutation morse apo tap-code per te vazhduar");
+            
+
+            }}
+        catch (Exception e)
+        {
+            System.out.println("Duhet te shkruani mjaftueshem argumente");
+            System.out.println(e.getMessage());
         }
 
-        if ("morse".equals(args[0])) {
 
-            if ("encrypt".equals(args[1])) {
-                MorseEncoder encoder=new MorseEncoder(args[2]);
-                encoder.OutputMorse();
-
-            } else if ("decrypt".equals(args[1])) {
-                MorseDecoder decoder=new MorseDecoder(args[2]);
-
-            }
-        }
-    }}
+    }
+}
