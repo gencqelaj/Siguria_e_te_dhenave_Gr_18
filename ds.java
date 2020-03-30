454d02de6b50d15029b49ec32140cf1fe2b0c375
kushtrimhaziri <58092838+kushtrimhaziri@users.noreply.github.com>
2020-03-30 12:26:38 +0200

    Update ds.java

--- ds.java
+++ ds.java
@@ -1,67 +1,71 @@
 public class ds {
-
     public static void main(String[] args) {
         try {
-            switch (args[0]) {
-                case "permutation":
-                    switch (args[1]) {
-                        case "encrypt":
+switch (args[0]) {
+    case "permutation":
+        Permutation permutation = new Permutation(args[2], args[3]);
+        if (args[1].equals("encrypt"))
+            permutation.encrypt();
+        else if (args[1].equals("decrypt"))
+            permutation.decrypt();
+        else
+            System.out.println("Ju lutem shtypni encrypt ose decrypt si argument te dyte");
 
-                            Permutation permutation = new Permutation(args[2], args[3]);
+        break;
 
-                            permutation.encrypt();
-                            break;
-                        case "decrypt":
-
-                            Permutation permutation1 = new Permutation(args[2], args[3]);
-                            permutation1.decrypt();
-                            break;
-                        default:
-                            System.out.println("Zgjedh si argument te dyte encrypt ose decrypt");
-                    }
-                    break;
-                    case "morse":
-                    switch (args[1]) {
-                        case "encrypt":
-                            MorseEncoder encoder = new MorseEncoder(args[2]);
-                            encoder.OutputMorse();
-                            break;
-                        case "decrypt":
-                            MorseDecoder decoder = new MorseDecoder(args[2]);
-                            break;
-                        default:
-                            System.out.println("Zgjedh si argument te dyte encrypt ose decrypt");
-                    }
-                    break;
-                    case "tapcode":
-
-                    String tipi = args[1];
-                    String input = args[2];
-                    TapCode tapObj = new TapCode(input);
-                    switch (tipi) {
-                        case "encode":
-                            tapObj.enkripto();
-                            break;
-                        case "decode":
-                           tapObj.dekripto();
-                            break;
-                        default:
-                            System.out.println("Zgjedh si argument te dyte encode ose decode");
-                            System.exit(1);
-                            
-                    }
-                    break;
-                default:
-                    System.out.println("Argumenti i pare eshte gabim , zgjedh permutation morse apo tap-code per te vazhduar");
-            
-
-            }}
-        catch (Exception e)
+    case "morse":
+        if (args[1].equals("encrypt"))
         {
-            System.out.println("Duhet te shkruani mjaftueshem argumente");
-            System.out.println(e.getMessage());
+            MorseEncoder encoder = new MorseEncoder(args[2]);
+            encoder.OutputMorse();
         }
+        else if (args[1].equals("decrypt"))
+        {
+            for (int i =0 ; i<args[2].length() ; i++)
+            {
+                if (args[2].charAt(i)=='-' || args[2].charAt(i)=='.' || args[2].charAt(i)==' ')
+                {
+                     MorseDecoder decoder = new MorseDecoder(args[2]);
+                }
+                else
+                    System.out.println("Teksti nuk duhet te permbaje karaktere tjere perpos - , . , ose space");
+                System.exit(0);
+            }
+
+        }
+        else
+            System.out.println("Ju lutem shtypni encrypt ose decrypt si argument te dyte per te ekzekutuar");
 
 
+
+        break;
+
+
+    case "tap-code":
+
+        if (args[1].equals("encrypt")) {
+            TapCode tapObj = new TapCode(args[2]);
+            tapObj.enkripto();
+        }
+        else if (args[1].equals("decrypt"))
+        {
+            TapCode tapObj = new TapCode(args[2]);
+            tapObj.dekripto();
+        }
+        else
+            System.out.println("Ju lutem shtypni encrypt ose decrypt per te ekzekutuar");
+
+        break;
+
+    default:
+        System.out.println("Ju lutem shtypni njeren nga komandat permutation morse apo tap-code si argument te pare");
+
+}
+
+
+} catch (Exception e )
+        {
+            System.out.println("Ju nuk keni shtypur gjithe argumentet e nevojshme");
+        }
     }
 }
