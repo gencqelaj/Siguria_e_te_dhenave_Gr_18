27da4d71fc8b5f0f95ea183a3b850494c74bf69f
visarahmeti <58608274+Visskey@users.noreply.github.com>
2020-04-27 16:52:25 +0200

    Update RSAKeyPairGenerator.java
    Ky funksion e merr string-un e dhene nga useri dhe e kthen ate ne nje file

--- RSAKeyPairGenerator.java
+++ RSAKeyPairGenerator.java
@@ -6,8 +6,37 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
+import javax.crypto.NoSuchPaddingException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 
 
 class RSAKeyPairGenerator {
+  
+   private static String Path = "C:\\\\Users\\\\Admin\\\\Desktop\\\\keys\\\\";
+
+    private static Document ConvertStringToDocumentBuilder(String stringBuilder)
+    {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = null;
+        Document doc = null;
+        try {
+            builder = factory.newDocumentBuilder();
+
+            //Parse the content to Document object
+            doc = builder.parse(new InputSource(new StringReader(stringBuilder)));
+
+        }
+        catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return doc;
+    }
 
 }
