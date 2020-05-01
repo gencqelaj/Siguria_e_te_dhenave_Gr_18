b540c5b9f13b516f0df5cb2aec439eb81c4713a0
gencqelaj <58531203+gencqelaj@users.noreply.github.com>
2020-05-02 00:21:59 +0200

    Update RSAKeyPairGenerator.java

--- RSAKeyPairGenerator.java
+++ RSAKeyPairGenerator.java
@@ -4,7 +4,24 @@ class RSAKeyPairGenerator{
     public String getPath(){
         return Path;
     }
-
+   private static void writeXmlDocumentToXmlFile(Document xmlDocument, String fileName)
+       {
+           TransformerFactory tf = TransformerFactory.newInstance();
+           Transformer transformer;
+           try {
+               transformer = tf.newTransformer();
+               FileOutputStream outStream = new FileOutputStream(new File(fileName));
+               transformer.transform(new DOMSource(xmlDocument), new StreamResult(outStream));
+           }
+           catch (TransformerException e)
+           {
+               e.printStackTrace();
+           }
+           catch (Exception e)
+           {
+               e.printStackTrace();
+           }
+       }
 
 
 
