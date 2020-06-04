fb1cf1dec1f4080548bc7f64f8fcfb349cec5b65
kushtrimhaziri <58092838+kushtrimhaziri@users.noreply.github.com>
2020-06-05 00:15:29 +0200

    Update faza3.java

--- faza3.java
+++ faza3.java
@@ -79,7 +79,19 @@ public class faza3 {
         }
         return doc;
     }
-  
+    private static void writeXmlDocumentToXmlFile(Document xmlDocument, String fileName) {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer;
+        try {
+            transformer = tf.newTransformer();
+            FileOutputStream outStream = new FileOutputStream(new File(fileName));
+            transformer.transform(new DOMSource(xmlDocument), new StreamResult(outStream));
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
   
   
   
