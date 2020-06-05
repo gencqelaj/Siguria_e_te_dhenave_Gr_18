5aafef45672368453834c14f7582c67d8bc963b9
visarahmeti <58608274+Visskey@users.noreply.github.com>
2020-06-05 22:09:23 +0200

    Update faza3.java

--- faza3.java
+++ faza3.java
@@ -142,5 +142,42 @@ public class faza3 {
 
         return k;
     }
+  static String getPassword(String prompt) {
+
+        String password = "";
+        ConsoleEraser consoleEraser = new ConsoleEraser();
+        System.out.print(prompt);
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        consoleEraser.start();
+        try {
+            password = in.readLine();
+        }
+        catch (IOException e){
+            System.out.println("Error trying to read your password!");
+            System.exit(1);
+        }
+
+        consoleEraser.halt();
+        System.out.print("\b ");
+
+        return password;
+    }
+    private static class ConsoleEraser extends Thread {
+        private boolean running = true;
+        public void run() {
+            while (running) {
+                System.out.print("\b ");
+                try {
+                    Thread.currentThread().sleep(1);
+                }
+                catch(InterruptedException e) {
+                    break;
+                }
+            }
+        }
+        public synchronized void halt() {
+            running = false;
+        }
+    }
   
   }
